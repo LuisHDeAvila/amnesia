2f866ea945e5e301e2e2109a9bad2ff0d007c671
elAche <IKPEdLzwoD26aKE@gmail.com>
2022-10-29 14:33:31 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Privacity tools
+Anonsurf installer for kali linux.
+
+```
+	sudo bash mask-installer.sh
+```
--- macchanger.sh
+++ macchanger.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # author: eleAche
-# version: 0.0 (esto es testing)
+# version: 0.0 (esto solo es un script)
 if [ $(id -u) -ne 0 ]; then
 	echo "This script must be ran as root"
 	exit 1
 fi
-ip link  | grep -v lo | grep -v link | awk -F: '{print $2}' | while read line; do 
+ip link  | grep -v lo | grep -v link | awk -F: '{print $2}' | while read line; do
   ifconfig $line down
   macchanger -r $line
-  ifconfig $line up 
+  ifconfig $line up
 done
-anonsurf start || anonsurf restart 
+anonsurf start || anonsurf restart || echo "please, run mask-installer.sh"
